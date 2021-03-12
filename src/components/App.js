ad33ae8828d1d26472fcdf7315ad806d535e897c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-12 16:53:22 +0100

    successfuly fetching data with help of 'axios' now is time to display that data

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,24 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSumbit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  onSearchSumbit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID WGYm1KAd29kz8gSbrSZMWUP0rjvpGeJkL1EeuysPW5o',
+      },
+    });
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSumbit} />
+        Found: {this.state.images.length} images.
       </div>
     );
   }
